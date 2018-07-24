d1bec96d4d934bce7c8f566d4d9ab598bd1e3982
Innocent Asiimwe <innocent.asiimwe@andela.com>
2018-07-25 02:25:09 +0300

    Add whitenoise middleware.

--- conduit/settings.py
+++ conduit/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'conduit.urls'
@@ -159,7 +161,3 @@ REST_FRAMEWORK = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_files"),
-]
